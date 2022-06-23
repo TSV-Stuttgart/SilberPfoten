a596944b901b612c67264782992909eea8c8e533
Sandro Coletti <scoletti@stigits.com>
2022-06-23 11:07:19 +0200

    fix: remove yarn lock

--- packages/mein.silberpfoten.de/next.config.js
+++ packages/mein.silberpfoten.de/next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 
 module.exports = withPWA({
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
   pwa: {
     dest: 'public',
     runtimeCaching,
