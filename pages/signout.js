fd7d14589e0de325cf591df2fcbe0668a6ebba49
Sandro Coletti <scoletti@stigits.com>
2022-12-06 19:21:06 +0100

    add signout

---
+++ pages/signout.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import db from '../lib/db'
+import logger from '../lib/logger'
+import getToken from '../lib/auth/getToken'
+
+export async function getServerSideProps(context) {
+  logger.info(`signout`)
+  
+  try {
+    logger.info(`signout | get token`)
+
+    const token = await getToken(context.req)
+
+    if (token?.user?.user_id) {
+      logger.info(`signout | delete session in database`)
+      const dbRequest = await db.query(`DELETE FROM dbo.session WHERE user_id = $1 RETURNING uuid`, [token.user.user_id])
+      logger.info(`signout | delete session in database | deleted | ${dbRequest.rowCount}`)
+    }
+
+    logger.info(`signout | cookie | invalidate`)
+    
+    context.res.setHeader('set-cookie',`session=deleted; SameSite=Lax; HttpOnly; Path=/; Expires=${new Date('1970-01-01').toUTCString()}`)
+
+    return {
+      redirect: {
+        destination: `/signin`,
+        statusCode: 302,
+      },
+    }
+
+  } catch(e) {
+    logger.info(`public | event | error | ${e}`)
+
+    return {
+      props: {}
+    }
+  }
+}
+
+export default function SignOut() {
+  
+  return <></>
+}
