a79f84d505de28dca478e178908b2894aa706fb8
Sandro Coletti <scoletti@stigits.com>
2022-12-09 09:46:30 +0100

    send email on activation. closes #7

--- pages/api/admin/member/activation.js
+++ pages/api/admin/member/activation.js
@@ -1,6 +1,7 @@
 import getToken from '../../../../lib/auth/getToken'
 import logger from '../../../../lib/logger'
 import db from '../../../../lib/db'
+import sendMail from '../../../../lib/sendMail'
 
 export default async function handler(request, response) {
 
@@ -12,7 +13,30 @@ export default async function handler(request, response) {
 
     logger.info(`api | admin | member | activation | db request`)
 
-    const dbRequest = await db.query(`UPDATE dbo.user SET activated_at = 'now()', activated_from_user = $1 WHERE user_id = $2`, [token.user.user_id, request.query.userId])
+    const dbRequest = await db.query(
+      `UPDATE dbo.user SET activated_at = 'now()', activated_from_user = $1 WHERE user_id = $2 RETURNING user_id, email, firstname, lastname`, 
+      [token.user.user_id, request.query.userId]
+    )
+
+    logger.info(`api | admin | member | activation | send welcome mail`)
+
+    if (dbRequest.rowCount > 0) {
+      const to = dbRequest.rows[0].email
+      const templateName = 'activationNotification'
+      const subject = 'Dein Profil wurde freigeschaltet'
+      const params = {
+        firstname: dbRequest.rows[0].firstname,
+        lastname: dbRequest.rows[0].lastname,
+      }
+
+      const sent = sendMail(to, subject, templateName, params)
+
+      if (sent.statusCode === 200) {
+        logger.info(`api | admin | member | activation | sent welcome mail`)
+      } else {
+        logger.info(`api | admin | member | activation | error sending email`)
+      }
+    }
 
     logger.info(`api | admin | member | activation | response`)
 
