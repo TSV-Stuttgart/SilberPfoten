28ed5995413f14e7eb350a5d7830a65f1989677d
thebluewin <52869419+thebluewin@users.noreply.github.com>
2023-05-30 15:14:52 +0200

    modify fixtures

--- config/postgres/010_fixtures.sql
+++ config/postgres/010_fixtures.sql
@@ -1,7 +1,9 @@
 DO $$
 DECLARE
     demoAdmin1UserId int;
+    demoAdmin2UserId int;
     demoHelper1UserId int;
+    demoHelper2UserId int;
     demoHelper2NotActivatedUserId int;
 BEGIN
 
@@ -34,6 +36,36 @@ BEGIN
     'ADMIN',
     'now()'
   ) ON CONFLICT DO NOTHING RETURNING user_id INTO demoAdmin1UserId;
+
+    INSERT INTO public.user (
+    gender,
+    firstname,
+    lastname,
+    email,
+    birthdate,
+    phone,
+    street,
+    street_number,
+    zipcode,
+    city,
+    job,
+    status,
+    activated_at
+  ) VALUES (
+    'male',
+    'Christian',
+    'Ruff',
+    'ruffchr@yahoo.de',
+    '1991-08-07',
+    '017620888842',
+    'Im Bäckerfeld',
+    '24',
+    '84072',
+    'Au i.d. Hallertau',
+    'Developer',
+    'ADMIN',
+    'now()'
+  ) ON CONFLICT DO NOTHING RETURNING user_id INTO demoAdmin2UserId;
 
   INSERT INTO public.user (
     gender,
@@ -76,6 +108,48 @@ BEGIN
     'now()',
     demoAdmin1UserId
   ) ON CONFLICT DO NOTHING RETURNING user_id INTO demoHelper1UserId;
+
+    INSERT INTO public.user (
+    gender,
+    firstname,
+    lastname,
+    email,
+    birthdate,
+    phone,
+    street,
+    street_number,
+    zipcode,
+    city,
+    job,
+    became_aware_through,
+    became_aware_through_other,
+    experience_with_animal,
+    experience_with_animal_other,
+    support_activity,
+    status,
+    activated_at,
+    activated_from_user
+  ) VALUES (
+    'male',
+    'Peter',
+    'Helfer',
+    'demo-silberpfoten-helfer2@stigits.com',
+    '1990-09-28',
+    '01771111111',
+    'Schwabstraße',
+    '36',
+    '73760',
+    'Ostfildern',
+    'Gärtner',
+    ARRAY['instagram','facebook','internet','press','friend','other'],
+    'Twitter',
+    ARRAY['dog','cat','small_animal','bird','other'],
+    'Schlangen',
+    ARRAY['go_walk','veterinary_trips','animal_care','events','baking_cooking','creative_workshop','public_relation','light_office_work','graphic_work'],
+    'USER',
+    'now()',
+    demoAdmin2UserId
+  ) ON CONFLICT DO NOTHING RETURNING user_id INTO demoHelper2UserId;
 
   INSERT INTO public.user (
     gender,
