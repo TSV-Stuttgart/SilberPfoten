21e809e5470bee00866a31059603b864f3caa453
thebluewin <52869419+thebluewin@users.noreply.github.com>
2023-06-22 14:10:52 +0200

    add filter and show zipcode in table

--- pages/admin/cases.js
+++ pages/admin/cases.js
@@ -9,7 +9,6 @@ import Loading from '../../components/Loading'
 import SessionExpired from '../../components/SessionExpired'
 import dynamic from 'next/dynamic'
 import ReactHtmlParser from 'react-html-parser'
-
 import 'react-quill/dist/quill.snow.css'
 import Link from 'next/link'
 
@@ -17,6 +16,8 @@ export default function Home() {
   const {mutate} = useSWRConfig()
   const {data: messages, error: messagesError} = useSWR(`/api/admin/cases`, (url) => fetch(url).then(r => r.json()))
   const {session} = useSession()
+
+  const [formZipcode, setFormZipcode] = useState('')
 
   useEffect(() => {
     import('bootstrap/js/dist/dropdown')
@@ -51,29 +52,42 @@ export default function Home() {
         </div>
 
         <div className="container mt-1 mb-3">
+          <div className="row mt-0 mb-1">
+            <div className="col-4 fw-bold"><span className="align-middle">Filtern nach:</span></div>
+          </div>
+          <div className="row mt-0">
+            <div className="col-4 fw-bold">
+              <input type="text" name="formSubject" className="form-control" placeholder="PLZ" value={formZipcode} onChange={(e) => setFormZipcode(e.target.value)} required />
+            </div>
+          </div>
+        </div>
+
+        <div className="container mb-3">
           <div className="row mt-2">
             <div className="col-12">
               <div className="p-3 bg-light rounded">
                 <div className="row">
+                  <div className="col-1 fw-bold border-end">PLZ</div>
                   <div className="col-6 fw-bold border-end">Betreff</div>
                   <div className="col-3 fw-bold border-end">Status</div>
-                  <div className="col-3 fw-bold">Erstellt am</div>
+                  <div className="col-2 fw-bold">Erstellt am</div>
                 </div>
               </div>
             </div>
           </div>
-          {messages.filter(m => m.message_type === 'case').map(message => <React.Fragment key={message.message_id}>
+          {messages.filter(m => m.message_type === 'case' && m.zipcode.startsWith(formZipcode)).map(message => <React.Fragment key={message.message_id}>
           <div className="row">
             <div className="col-12">
               <Link href={`/admin/case/${message.message_id}/${slugify(message.subject, {lower: true})}`} className="text-decoration-none text-dark">
               <div className="px-3 py-1">
                 <div className="row align-items-center">
+                  <div className="col-1 border-end">{message.zipcode}</div>
                   <div className="col-6 border-end">{message.subject}</div>
                   <div className="col-3 border-end">
                     {!message.accepted_case_users ? <span className="fw-normal text-success">Bedarf für Hilfe vorhanden</span> : null}
                     {message.accepted_case_users?.length > 0 ? <span className="fw-normal text-success">{message.accepted_case_users?.length} Bewerber</span> : null}
                   </div>
-                  <div className="col-3">{new Date(message.created_at).toLocaleDateString('de-DE', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})} Uhr</div>
+                  <div className="col-2">{new Date(message.created_at).toLocaleDateString('de-DE', {day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit'})} Uhr</div>
                 </div>
               </div>
               </Link>
