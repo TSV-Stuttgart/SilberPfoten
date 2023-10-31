03d8340dfc51b9370cc96af67ad9322d602158f3
thebluewin <52869419+thebluewin@users.noreply.github.com>
2023-10-31 09:52:21 +0100

    changed: show members without email

--- pages/admin/scriptNewsletter.js
+++ pages/admin/scriptNewsletter.js
@@ -41,7 +41,7 @@ export default function Users() {
 
     let counter = 0
     /// filter bei users für deaktivated newsletter
-    for (const user of users?.filter(u => filter === 'with_bounce' ? u.newsletter_bounced : !u.newsletter_bounced)) {
+    for (const user of users?.filter(u => u.email && filter === 'with_bounce' ? u.newsletter_bounced : !u.newsletter_bounced)) {
 
       if(pauseSending) break
 
@@ -84,8 +84,8 @@ export default function Users() {
           <div className="col-12 col-md-6 text-end">
             <div className="fw-bold h3">
               <div className="btn-group" role="group" aria-label="Basic example">
-                <button onClick={() => setFilter('without_bounce')} type="button" className="btn btn-light">Mitglieder ohne Bounce {users.filter(u => !u.newsletter_bounced).length > 0 ? <span className="small bg-secondary text-white fw-bold rounded px-2">{users.filter(u => !u.newsletter_bounced).length}</span> : null}</button>
-                <button onClick={() => setFilter('with_bounce')} type="button" className="btn btn-light">Mitglieder mit Bounce {users.filter(u => u.newsletter_bounced).length > 0 ? <span className="small bg-secondary text-white fw-bold rounded px-2">{users.filter(u => u.newsletter_bounced).length}</span> : null}</button>
+                <button onClick={() => setFilter('without_bounce')} type="button" className="btn btn-light">Mitglieder ohne Bounce {users.filter(u => u.email && !u.newsletter_bounced).length > 0 ? <span className="small bg-secondary text-white fw-bold rounded px-2">{users.filter(u => u.email && !u.newsletter_bounced).length}</span> : null}</button>
+                <button onClick={() => setFilter('with_bounce')} type="button" className="btn btn-light">Mitglieder mit Bounce {users.filter(u => u.email && u.newsletter_bounced).length > 0 ? <span className="small bg-secondary text-white fw-bold rounded px-2">{users.filter(u => u.email && u.newsletter_bounced).length}</span> : null}</button>
               </div>
             </div>
           </div>
@@ -112,8 +112,9 @@ export default function Users() {
         }
         </div>
       </div>
-
-      {users?.filter(u => filter === 'with_bounce' ? u.newsletter_bounced : !u.newsletter_bounced)?.length > 0 ? <>
+      
+      <h3 className="mt-3">Mitglieder mit E-Mail</h3>
+      {users?.filter(u => filter === 'with_bounce' ? u.email && u.newsletter_bounced : u.email && !u.newsletter_bounced)?.length > 0 ? <>
         <div className="container mt-2">
           <div className="row mb-1">
             <div className="col-12">
@@ -127,7 +128,56 @@ export default function Users() {
               </div>
             </div>
           </div>
-          {users?.filter(u => filter === 'with_bounce' ? u.newsletter_bounced : !u.newsletter_bounced)?.map(user => <div className="row" key={`${user.user_id}`}>
+          {users?.filter(u => filter === 'with_bounce' ? u.email && u.newsletter_bounced : u.email && !u.newsletter_bounced)?.map(user => <div className="row" key={`${user.user_id}`}>
+            <div className="col-12">
+              <div className="px-2 py-1">
+                <div className="row mb-1">
+                  <div className="col-1">{user.user_id}</div>
+                  <div className="col-3">
+                    <Link href={`/user/${user.user_id}/${slugify(`${user.lastname}-${user.firstname}`, {lower: true})}`}>
+                      <div className="text-secondary">{user.lastname}, {user.firstname} {user.status === 'ADMIN' ? <i className="bi bi-person-fill-gear ms-1" style={{fontSize: 16}}></i> : null}</div>
+                    </Link>
+                    {/* {user.activated_at ? <i className="ms-1 bi bi-patch-check-fill text-secondary"></i> : null} */}
+                  </div>
+                  {/* // new Date(user.activated_at).toLocaleDateString('de-DE', {day: '2-digit', month: '2-digit', year: '2-digit'}) : <>nein</>} */}
+                  <div className="col-7">
+                    {user.email}
+                    {/* {new Date(user.created_at).toLocaleDateString('de-DE', {day: '2-digit', month: '2-digit', year: 'numeric'})} */}
+                  </div>
+                  <div className="col-1 text-end">
+                      
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>)}
+        </div>
+      </> : <>
+        <div className="container mt-2">
+          <div className="row mb-1">
+            <div className="col-12">
+              <div className="p">Keine weiteren Einträge</div>
+            </div>
+          </div>
+        </div>
+      </>}
+
+      <h3 className="mt-3">Mitglieder ohne E-Mail</h3>
+      {users?.filter(u => !u.email)?.length > 0 ? <>
+        <div className="container mt-2">
+          <div className="row mb-1">
+            <div className="col-12">
+              <div className="bg-light rounded p-2">
+                <div className="row">
+                  <div className="col-1 border-end fw-bold">#</div>
+                  <div className="col-3 border-end fw-bold">Name</div>
+                  <div className="col-7 fw-bold">E-Mail</div>
+                  <div className="col-1 text-end"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          {users?.filter(u => !u.email)?.map(user => <div className="row" key={`${user.user_id}`}>
             <div className="col-12">
               <div className="px-2 py-1">
                 <div className="row mb-1">
