b799c89c6fe7395fc2d16caae582c926884c8c03
thebluewin <52869419+thebluewin@users.noreply.github.com>
2024-04-29 12:09:58 +0200

    fixed: typo in imports

--- pages/admin/users.js
+++ pages/admin/users.js
@@ -1,4 +1,4 @@
-import React, {use, useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
 import useSWR, {useSWRConfig} from 'swr'
 import Error from '../../components/Error'
