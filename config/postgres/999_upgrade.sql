1842e5e9ffde7fd75162aa4430d0c9e2a59c1307
thebluewin <52869419+thebluewin@users.noreply.github.com>
2024-06-06 09:26:23 +0200

    added: create audit table

--- config/postgres/999_upgrade.sql
+++ config/postgres/999_upgrade.sql
@@ -1,22 +1,9 @@
-DO $$
-DECLARE
-    CURRENT_APP_VERSION varchar(16);
-    NEXT_APP_VERSION varchar(16);
 
-    FREE_RECORD_VARIABLE_1 record;
-
-    FREE_INT_VARIABLE_1 integer;
-    FREE_INT_VARIABLE_2 integer;
-    FREE_INT_VARIABLE_3 integer;
-BEGIN
-
-    -- Audits und History für Tabellen anlegen (Ticket #40)
-    CREATE TABLE IF NOT EXISTS public.audit (
-      audit_id BIGSERIAL,
-      action_type VARCHAR(255),
-      data_json TEXT,
-      created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
-      PRIMARY KEY(audit_id)
-    );
-
-END $$;
+-- Audits und History für Tabellen anlegen (Ticket #40)
+CREATE TABLE IF NOT EXISTS public.audit (
+  audit_id BIGSERIAL,
+  action_type VARCHAR(255),
+  data_json TEXT,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+  PRIMARY KEY(audit_id)
+);
